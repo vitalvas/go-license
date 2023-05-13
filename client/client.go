7805fcc93c3b5a622d6fc50d383bcff3dd223ccd
Vitaliy Vasilenko <me@vitalvas.com>
2023-05-13 14:02:09 -0400

    chore: move license client

--- client/client.go
+++
@@ -1,6 +0,0 @@
-package client
-
-type Client struct {
-	DNSHosts     []string
-	APIEndpoints []string
-}
--- client/verify_api.go
+++
@@ -1,8 +0,0 @@
-package client
-
-import "github.com/vitalvas/go-license/license"
-
-func (c *Client) APIVerify(lic *license.License) bool {
-
-	return false
-}
