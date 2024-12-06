d5d84f4a854bfce0cab5f345d96f1e2d68d921b5
Vitaliy Vasylenko <me@vitalvas.com>
2024-12-05 23:08:41 -0500

    fix: drop unused code

--- examples/generate/main.go
+++ examples/generate/main.go
@@ -11,72 +11,73 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/vitalvas/go-license/license"
-	"github.com/vitalvas/go-license/license/licenseutil"
 )
 
-var privateKey ed25519.PrivateKey
-var publicKey ed25519.PublicKey
-var err error
-
-func init() {
-	publicKey, privateKey, err = ed25519.GenerateKey(rand.Reader)
+func main() {
+	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func main() {
-	verify(generate())
+	generateKey := generate(privateKey)
+
+	fmt.Println(strings.TrimSpace(string(generateKey)))
+
+	verify(generateKey, publicKey)
 }
 
 type licenseContent struct {
-	Org struct {
-		Name  string `json:"name"`
-		Email string `json:"email"`
-	} `json:"org"`
-	Features []string       `json:"features"`
-	Limits   map[string]int `json:"limits"`
+	Org      licenseContentOrg `json:"org"`
+	Features []string          `json:"features"`
+	Limits   map[string]int    `json:"limits"`
 }
 
-func generate() []byte {
-	lic := licenseutil.NewGenerate()
-	lic.LoadPrivateKey(privateKey)
+type licenseContentOrg struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
-	licData := licenseContent{}
+func generate(privateKey ed25519.PrivateKey) []byte {
+	now := time.Now()
 
-	licData.Org.Name = "ACME inc"
-	licData.Org.Email = "acme@example.com"
-
-	licData.Features = []string{
-		"api",
-		"api.auth.google",
-		"api.auth.ldap",
+	lic := license.License{
+		ID:        "e79ac885-de88-4c09-a2bc-7eca47a069bf",
+		IssuedAt:  now.Truncate(time.Hour * 24).UTC().Unix(),
+		ExpiredAt: now.Add(365 * 24 * time.Hour).Truncate(time.Hour * 24).UTC().Unix(),
 	}
 
-	licData.Limits = map[string]int{
-		"core.users.max":                 123,
-		"module.auth.ldap.directory.max": 1,
+	licData := licenseContent{
+		Org: licenseContentOrg{
+			Name:  "ACME inc",
+			Email: "acme@example.com",
+		},
+		Features: []string{
+			"api",
+			"api.auth.google",
+			"api.auth.ldap",
+		},
+		Limits: map[string]int{
+			"core.users.max":                 123,
+			"module.auth.ldap.directory.max": 1,
+		},
 	}
 
-	lic.SetID("e79ac885-de88-4c09-a2bc-7eca47a069bf")
-
-	lic.SetExpired(time.Now().Add(14 * 24 * time.Hour))
-
-	if err := lic.SetData(licData); err != nil {
-		log.Fatal(err)
-	}
-
-	key, err := lic.GetLicenseKey()
+	payload, err := json.Marshal(licData)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(strings.TrimSpace(string(key)))
+	lic.Data = payload
+
+	key, err := lic.Encode(privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return key
 }
 
-func verify(licenseKey []byte) {
+func verify(licenseKey []byte, publicKey ed25519.PublicKey) {
 	lic, err := license.Decode(licenseKey, publicKey)
 	if err != nil {
 		log.Fatal(err)
