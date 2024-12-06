43c6fa0ecd9a2d0a8ccfb8973951c389f1138903
Vitaliy Vasylenko <me@vitalvas.com>
2024-12-06 00:41:20 -0500

    chore: small refactor TestGetFingerprint

--- license/license_test.go
+++ license/license_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/base64"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestExpired(t *testing.T) {
@@ -25,23 +27,32 @@ func TestExpired(t *testing.T) {
 }
 
 func TestGetFingerprint(t *testing.T) {
-	license := &License{}
+	t.Run("Valid", func(t *testing.T) {
+		license := &License{
+			ID: "123",
+		}
 
-	hash, err := license.GetFingerprint()
-	if err != nil {
-		t.Error(err)
-	}
+		hash, err := license.GetFingerprint()
+		if err != nil {
+			t.Error(err)
+		}
 
-	decoded, err := base64.RawURLEncoding.DecodeString(hash)
-	if err != nil {
-		t.Error(err)
-	}
+		decoded, err := base64.RawURLEncoding.DecodeString(hash)
+		if err != nil {
+			t.Error(err)
+		}
 
-	if len(decoded) != 32 {
-		t.Errorf("Expect fingerprint to be 32 bytes")
-	}
+		assert.Len(t, decoded, 32)
 
-	if hash != "RBNvo1WzZ4oRRq0W9-hknpT7T8If536DEMBg9hyq_4o" {
-		t.Errorf("Expect fingerprint to match")
-	}
+		assert.Equal(t, "ECcsha9GmtgfZtn17D76cO4Kx7kfqeBd2prdVKYGID4", hash)
+	})
+
+	t.Run("Empty", func(t *testing.T) {
+		license := &License{}
+
+		hash, err := license.GetFingerprint()
+		assert.Nil(t, err)
+
+		assert.Equal(t, "RBNvo1WzZ4oRRq0W9-hknpT7T8If536DEMBg9hyq_4o", hash)
+	})
 }
